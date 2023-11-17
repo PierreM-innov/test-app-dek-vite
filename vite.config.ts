67e7641478e09fdd3ab4a4380f1f415beb093b0f
Pierre Mériel <pierre.meriel@innov-atm.com>
2023-11-17 19:36:25 +0100

    test manual registration

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,23 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {VitePWA} from "vite-plugin-pwa";
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
     plugins: [react(),
         basicSsl(),
         VitePWA({
             registerType: 'autoUpdate',
         })],
+    build: {
+        outDir: "./dist",
+        lib: {
+            entry: path.resolve("./firebase-messaging-sw.js"),
+            fileName: "firebase-messaging-sw",
+            formats: ["es"]
+        },
+        emptyOutDir: false
+    },
 })
