b0843e64894611e47eee0b9d8b141e2de6693f9b
Pierre Mériel <pierre.meriel@innov-atm.com>
2023-11-20 15:39:07 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TestWakeLockComponent from "./components/TestWakeLockComponent.tsx";
+import {useEffect} from "react";
 
 
 function App() {
 
+  useEffect(() => {
+    const registerServiceWorker = async () => {
+      if ('serviceWorker' in navigator) {
+        try {
+          const registration = await navigator.serviceWorker.register('/sw.js');
+          console.log('SW registered:', registration);
+        } catch (error) {
+          console.error('SW registration failed:', error);
+        }
+      }
+    };
+
+    registerServiceWorker();
+  }, []);
 
 
-    return (
+  return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-if ('serviceWorker' in navigator) {
-        try {
-            const registration = await navigator.serviceWorker.register('/sw.js');
-            console.log('SW registered:', registration);
 
-        } catch (error) {
-            console.error('SW registration failed:', error);
-        }
-}
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
