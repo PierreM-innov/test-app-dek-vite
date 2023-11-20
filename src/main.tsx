05de49a9c5bf58174b18af5eb83d0390046a841d
Pierre Mériel <pierre.meriel@innov-atm.com>
2023-11-20 14:34:34 +0100

    vapid

--- src/main.tsx
+++ src/main.tsx
@@ -14,28 +14,38 @@ const firebaseConfig = {
 };
 initializeApp(firebaseConfig);
 
-// function urlBase64ToUint8Array(base64String:string) {
-//     var padding = '='.repeat((4 - base64String.length % 4) % 4);
-//     var base64 = (base64String + padding)
-//         .replace(/\-/g, '+')
-//         .replace(/_/g, '/');
-//
-//     var rawData = window.atob(base64);
-//     var outputArray = new Uint8Array(rawData.length);
-//
-//     for (var i = 0; i < rawData.length; ++i) {
-//         outputArray[i] = rawData.charCodeAt(i);
-//     }
-//     return outputArray;
-// }
+function urlBase64ToUint8Array(base64String:string) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
 
 
 if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then((registration) => {
+    window.addEventListener('load', async () => {
+        try {
+            const registration = await navigator.serviceWorker.register('/sw.js');
             console.log('SW registered:', registration);
-        }).catch((registrationError) => {
-            console.log('SW registration failed:', registrationError);
-        });
+
+            const subscription = await registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array('BLfHnz1uJeoy9zHHgnxFE1jcL5zn_HOeqbS1LDkHkwMcDpXxjmDrOvyCmG21f6qs0eVoaO4QWP0Rtii2D98cU3I'),
+            });
+
+            console.log('Push subscription:', JSON.stringify(subscription));
+        } catch (error) {
+            console.error('SW registration failed:', error);
+        }
+    });
 }
 
 
