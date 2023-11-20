62dba04fe4638bcd719d869f172fbab19175d2be
Pierre Mériel <pierre.meriel@innov-atm.com>
2023-11-20 10:47:24 +0100

    new try

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {getMessaging, getToken} from "firebase/messaging";
+// import {getMessaging, getToken} from "firebase/messaging";
 import {initializeApp} from "firebase/app";
 const firebaseConfig = {
     apiKey: "AIzaSyBI8PQhRA10G_WGchbvdr5kOsKcZmZ5CRs",
@@ -14,7 +14,22 @@ const firebaseConfig = {
     measurementId: "G-JVJLMR4EZS"
 };
 const firebaseApp = initializeApp(firebaseConfig);
-    const messaging = getMessaging(firebaseApp);
+    // const messaging = getMessaging(firebaseApp);
+
+function urlBase64ToUint8Array(base64String:string) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
 
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
@@ -22,14 +37,22 @@ if ("serviceWorker" in navigator) {
             import.meta.env.MODE === 'production' ? '/sw.js' : '/dev-sw.js?dev-sw',
             { type: import.meta.env.MODE === 'production' ? 'classic' : 'module' }
         )
-        .then((registration) => {
-            getToken(messaging, {
-                vapidKey: 'BCyo8NEIzJ8HGo_B9L1lG2_VMew4tuwgiaI2uhpMxPK3fUPd3AMXx6dnFENt0UT08HcNRW9y_pf97DnYO6wfAsY',
-                serviceWorkerRegistration : registration
-            })
-                .then((currentToken) => {
-                   console.log("current", currentToken)
-                });
+        .then(function (registration) {
+            const subscribeOptions = {
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(
+                    'BCyo8NEIzJ8HGo_B9L1lG2_VMew4tuwgiaI2uhpMxPK3fUPd3AMXx6dnFENt0UT08HcNRW9y_pf97DnYO6wfAsY',
+                ),
+            };
+
+            return registration.pushManager.subscribe(subscribeOptions);
+        })
+        .then(function (pushSubscription) {
+            console.log(
+                'Received PushSubscription: ',
+                JSON.stringify(pushSubscription),
+            );
+            return pushSubscription;
         });
 }
 
