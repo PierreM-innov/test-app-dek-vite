9117de119f7c48a3162e92dfcf847b7c05ed5de5
Pierre Mériel <pierre.meriel@innov-atm.com>
2023-11-20 15:33:38 +0100

    clean

--- src/main.tsx
+++ src/main.tsx
@@ -2,50 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {initializeApp} from "firebase/app";
-const firebaseConfig = {
-    apiKey: "AIzaSyBI8PQhRA10G_WGchbvdr5kOsKcZmZ5CRs",
-    authDomain: "test-dk-app.firebaseapp.com",
-    projectId: "test-dk-app",
-    storageBucket: "test-dk-app.appspot.com",
-    messagingSenderId: "576794172305",
-    appId: "1:576794172305:web:2562931c0826439a3f5830",
-    measurementId: "G-JVJLMR4EZS"
-};
-initializeApp(firebaseConfig);
-
-function urlBase64ToUint8Array(base64String:string) {
-    var padding = '='.repeat((4 - base64String.length % 4) % 4);
-    var base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
-
-    var rawData = window.atob(base64);
-    var outputArray = new Uint8Array(rawData.length);
-
-    for (var i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-}
-
 
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
         try {
             const registration = await navigator.serviceWorker.register('/sw.js');
             console.log('SW registered:', registration);
 
-            const subscription = await registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: urlBase64ToUint8Array('BI_-vdRg25ucEoPrL6f8yLgayiP5ACZ9f58zpsBQagF0i30VkDKGGHpS-BrLT7CdgKKFG3wfwlgVQrxUy0s-_4E'),
-            });
-
-            console.log('Push subscription:', JSON.stringify(subscription));
         } catch (error) {
             console.error('SW registration failed:', error);
         }
-    });
 }
 
 
